2afbb52d8327a0809e98c5b989f94d8137c40d3b
Aaron Bassett <aaronbassett@gmail.com>
2026-06-17 00:02:24 +0100

    gauge v0.1.0

---
+++ Formula/gauge.rb
@@ -0,0 +1,62 @@
+class Gauge < Formula
+  desc "Gauge client: authenticated query access, a terminal dashboard (TUI), and an MCP server for privacy-first developer telemetry."
+  homepage "https://github.com/aaronbassett/gauge"
+  version "0.1.0"
+  if OS.mac?
+    if Hardware::CPU.arm?
+      url "https://github.com/aaronbassett/gauge/releases/download/v0.1.0/gauge-client-aarch64-apple-darwin.tar.xz"
+    end
+  end
+  if OS.linux?
+    if Hardware::CPU.arm?
+      url "https://github.com/aaronbassett/gauge/releases/download/v0.1.0/gauge-client-aarch64-unknown-linux-gnu.tar.xz"
+    end
+    if Hardware::CPU.intel?
+      url "https://github.com/aaronbassett/gauge/releases/download/v0.1.0/gauge-client-x86_64-unknown-linux-gnu.tar.xz"
+    end
+  end
+  license any_of: ["MIT", "Apache-2.0"]
+
+  BINARY_ALIASES = {
+    "aarch64-apple-darwin": {},
+    "aarch64-unknown-linux-gnu": {},
+    "x86_64-unknown-linux-gnu": {}
+  }
+
+  def target_triple
+    cpu = Hardware::CPU.arm? ? "aarch64" : "x86_64"
+    os = OS.mac? ? "apple-darwin" : "unknown-linux-gnu"
+
+    "#{cpu}-#{os}"
+  end
+
+  def install_binary_aliases!
+    BINARY_ALIASES[target_triple.to_sym].each do |source, dests|
+      dests.each do |dest|
+        bin.install_symlink bin/source.to_s => dest
+      end
+    end
+  end
+
+  def install
+    if OS.mac? && Hardware::CPU.arm?
+      bin.install "gauge"
+    end
+    if OS.linux? && Hardware::CPU.arm?
+      bin.install "gauge"
+    end
+    if OS.linux? && Hardware::CPU.intel?
+      bin.install "gauge"
+    end
+
+    install_binary_aliases!
+
+    # Homebrew will automatically install these, so we don't need to do that
+    doc_files = Dir["README.*", "readme.*", "LICENSE", "LICENSE.*", "CHANGELOG.*"]
+    leftover_contents = Dir["*"] - doc_files
+
+    # Install any leftover files in pkgshare; these are probably config or
+    # sample files.
+    pkgshare.install(*leftover_contents) unless leftover_contents.empty?
+  end
+end
